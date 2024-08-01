6be7faaec39c1332c2e58b915c8ce5b86b878249
JoelDurafourd <joel.durafourd@gmail.com>
2024-08-01 14:51:19 +0900

    included Javascript and some basic functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const aboutMePic = document.querySelector(".about-me-pic")
+const viewBox = document.querySelector('.view')
+
+function navigate(page) {
+  aboutMePic.classList.add("d-none");
+  fetch(`${page}.html`)
+      .then(response => response.text())
+      .then(html => {
+        viewBox.innerHTML = html;
+      })
+      .catch(err => console.error('Error loading page:', err));
+}
+
+// function overviewScript() {
+//   fetch(`overview.html`)
+//   .then(response => response.text())
+//   .then(html => {
+//       document.querySelector('.view').innerHTML = html;
+//   })
+//   .catch(err => console.error('Error loading page:', err));
+
+//   // overview.classList.remove("d-none");
+// }
