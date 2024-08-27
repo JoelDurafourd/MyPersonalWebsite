ac903e17c61871f4377d1a9663eba9c3b3312099
Joel Durafourd <joel.durafourd@gmail.com>
2024-08-27 17:11:08 +0900

    added tab bar information, added languages and a few other bits and pieces

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const aboutMePic = document.querySelector(".about-me-pic")
-const viewBox = document.querySelector('.view')
+const viewBox = document.querySelector(".view")
+console.log("JavaScript file is loaded.");
+const myAge = document.getElementById("my-age");
+myAge.textContent += "31";
 
 function navigate(page) {
   aboutMePic.classList.add("d-none");
@@ -10,6 +13,24 @@ function navigate(page) {
       })
       .catch(err => console.error('Error loading page:', err));
 }
+
+function myAgeScript() {
+  const birthDate = new Date(birth);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+
+  // Adjust age if the birth date hasn't occurred yet this year
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  myAge.textContent = age;
+}
+
+// document.getElementById("my_label").innerHTML = myAgeScript();
+
 
 // function overviewScript() {
 //   fetch(`overview.html`)
